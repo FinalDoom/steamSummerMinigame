a9656acdb558cee1727f30ca74105afd42026f0a
Nate Moseley <FinalDoom@gmail.com>
2015-06-14 16:26:31 -0400

    Version again

--- autoUpgradeBuyer.user.js
+++ autoUpgradeBuyer.user.js
@@ -2,7 +2,7 @@
 // @name Steam Monster Minigame Auto-upgrade
 // @namespace https://github.com/wchill/steamSummerMinigame
 // @description A script that buys upgrades in the Steam Monster Minigame for you.
-// @version 1.0.3
+// @version 1.0.4
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
